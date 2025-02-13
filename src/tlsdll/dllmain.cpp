490c75129fca48707e38a4d595559fb6d7720510
xifil <iamspaceyt@gmail.com>
2025-02-13 16:29:32 +0000

    format tlsdll

--- src/tlsdll/dllmain.cpp
+++ src/tlsdll/dllmain.cpp
@@ -4,15 +4,12 @@
 #define TLS_PAYLOAD_SIZE 0x2000
 thread_local char tls_data[TLS_PAYLOAD_SIZE];
 
-__declspec(dllexport) void* get_tls_data()
-{
+__declspec(dllexport) void* get_tls_data() {
 	return &tls_data[0];
 }
 
-int WINAPI _DllMainCRTStartup(const HMODULE module, const unsigned long reason, void*)
-{
-	if (reason == DLL_PROCESS_ATTACH)
-	{
+int WINAPI _DllMainCRTStartup(const HMODULE module, const unsigned long reason, void*) {
+	if (reason == DLL_PROCESS_ATTACH) {
 		DisableThreadLibraryCalls(module);
 	}
 
